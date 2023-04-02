0b4311380e5e80320e5bea0d9892af32f1faa93b
amressam00 <47486312+amressam00@users.noreply.github.com>
2023-04-03 00:02:38 +0200

    Add files via upload

---
+++ struct_Global.h
@@ -0,0 +1,22 @@
+
+
+
+#include<stdlib.h>
+#include"STD_TYPES.h"
+typedef struct patientc
+{
+	u8 name[20];
+   u8 age;
+    u8 gender[20];
+   u32 ID;
+   u8 slot;
+   struct patient *next;
+}patient;
+patient *head;
+u8 a=0,aa=0;
+u8 b=0,bb=0;
+u8 d=0,dd=0;
+u8 e=0,ee=0;
+u8 f=0,ff=0;
+u8 c,w;
+u8 so,id2,z=0;
